6abf41fc07bb4bc4013af0dc483f4406e14f7cf6
Kegan Dougal <kegan@matrix.org>
2015-03-08 14:32:35 +0000

    Cleanup

--- config.sample.js
+++
@@ -1,7 +0,0 @@
-var config = {};
-config.homeServerUrl = "http://localhost:8008";
-config.applicationServiceUrl = "http://localhost:3500";
-config.applicationServiceToken = "1234567890";
-config.port = 3500;
-
-module.exports = config;

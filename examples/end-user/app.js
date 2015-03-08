2237b039c2f723e7ec65f40cc4e5c18fbf186549
Kegan Dougal <kegan@matrix.org>
2015-03-08 14:19:56 +0000

    Add usage examples

---
+++ examples/end-user/app.js
@@ -0,0 +1,13 @@
+var appservice = require("matrix-appservice");
+var logging = require("matrix-appservice-logging");
+
+appservice.registerServices([
+{
+    service: logging,
+    hs: "http://localhost:8008",
+    token: "1234567890",
+    as: "http://localhost:3500",
+    port: 3500
+}
+]);
+appservice.runForever();

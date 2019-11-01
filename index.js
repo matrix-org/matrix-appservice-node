882c80aef9a549f8eb2d418c9b06d06843091366
Half-Shot <will@half-shot.uk>
2019-11-01 15:28:41 +0000

    Add declarations for js files

--- index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-module.exports.AppService = require("./lib/app-service").AppService;
-module.exports.AppServiceRegistration = require("./lib/app-service-registration").AppServiceRegistration;
